6a44f8c750381fd1736de35a44614060c24e86f3
fagneraureliano <fagner2aureliano@gmail.com>
2022-05-10 14:57:41 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import  Widget  from "./src/components/Widget";
+import { theme } from "./src/theme";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import { useState, useEffect, useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_500Medium,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{ flex: 1, backgroundColor: theme.colors.background }}
+      onLayout={onLayoutRootView}
+    >
+      <StatusBar 
+        style="light" 
+        backgroundColor="transparent" 
+        translucent 
+      />
+
+      <Widget />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
