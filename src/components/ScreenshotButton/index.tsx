1d0054d874acc07ddff107429dfaa0bdf7b06b4f
fagneraureliano <fagner2aureliano@gmail.com>
2022-05-11 22:40:26 -0300

    feat(form) impl components de form e camera

---
+++ src/components/ScreenshotButton/index.tsx
@@ -0,0 +1,41 @@
+import { Camera, Trash } from "phosphor-react-native";
+import React from "react";
+import { View, TouchableOpacity, Image } from "react-native";
+import { theme } from "../../theme";
+
+import { styles } from "./styles";
+
+interface Props {
+  screenshot: string | null;
+  onTakeShot: () => void;
+  onRemoveShot: () => void;
+}
+
+export function ScreenshotButton({
+  screenshot,
+  onTakeShot,
+  onRemoveShot,
+}: Props) {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={screenshot ? onRemoveShot : onTakeShot}
+    >
+      {screenshot ? (
+        <Trash
+            size={22}
+            weight="fill"
+            color={theme.colors.text_secondary}
+            style={styles.removeIcon}
+           />
+      ) : (
+        <Camera 
+            size={22} 
+            color={theme.colors.text_secondary} 
+            weight="bold"
+            
+        />
+      )}
+    </TouchableOpacity>
+  );
+}
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -7,6 +7,10 @@ import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 import { styles } from "./styles";
 import { theme } from "../../theme";
 import { Options } from "../Options";
+import { Form } from "../Form";
+import { feedbackTypes } from "../../utils/feedbackTypes";
+
+export type FeedbackType = keyof typeof feedbackTypes
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -30,7 +34,9 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Form
+        feedbackType="BUG"
+        />
       </BottomSheet>
     </>
   );
